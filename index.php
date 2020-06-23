7816c4683e7167ccc957ac58ab48691738642d7b
Wibowo Dwi <50950100+wibowodwi@users.noreply.github.com>
2020-06-23 22:08:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travelia</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-left">
+          <a href="#">HOME</a>
+          <a href="#">TENTANG</a>
+          <img class="logo" src="logo.png">
+        </div>
+        <div class="header-right">
+          <a href="#" class="fa fa-twitter"></a>
+          <a href="#" class="fa fa-facebook"></a>
+        </div>
+    </header>
+     <div class="top-wrapper">
+      <div class="container">
+        <h1>TRAVELIA.CO</h1>
+        <h1>Travel Menjadi</h1>
+        <h1> Lebih Mudah.</h1>
+        <p>Travelia adalah agen perjalanan professional.</p>
+        <p>Kami menawarkan perjalanan ke penjuru dunia tanpa batas.</p>
+        <div class="btn-wrapper">
+          <a href="#" class="btn read">Read More</a>
+        </div>
+      </div>
+    </div>
+    <div class="test-wrapper">
+      <div class="container">
+        <div class="heading">
+          <h2>Testimoni</h2>
+        </div>
+        <div class="testi">
+          <div class="testimoni">
+            <div class="testimoni-icon">
+              <img src="testimoni1.jpg">
+               <p>Gn. Merapi</p>
+            </div>
+             <p class="text-contents">Gunung Merapi (ketinggian puncak 2.930 mdpl, per 2010) adalah gunung berapi di bagian tengah Pulau Jawa dan merupakan salah satu gunung api teraktif di Indonesia. </p>
+          </div>
+          <div class="testimoni">
+            <div class="testimoni-icon">
+              <img src="testimoni2.jpg">
+              <p>Gn. Merbabu</p>
+            </div>
+            <p class="text-contents">Gunung Merbabu adalah gunung api yang bertipe Strato (lihat Gunung Berapi) yang terletak secara geografis pada 7,5° LS dan 110,4° BT.</p>
+          </div>
+          <div class="testimoni">
+            <div class="testimoni-icon">
+              <img src="testimoni3.jpg">
+              <p>Halmahera</p>
+            </div>
+            <p class="text-contents">Halmahera (juga Jilolo atau Gilolo) adalah pulau terbesar di Kepulauan Maluku. Pulau ini merupakan bagian dari provinsi Maluku Utara, Indonesia. .</p>
+          </div>
+          
+          <div class="clear"></div>
+        </div>
+      </div>
+    </div>
+    <footer>
+    <div class="member-wrapper">
+      <div class="container">
+        <div class="heading">
+          <h2>Apakah anda siap untuk menjadi petualang sejati?</h2>
+          <h3>Ayo menjadi salah satu bagian dari member kami!</h3>
+        </div>
+        <span class="btn member">Daftar manjadi Member</span>
+      </div>
+      <img src="logo.png">
+      <p>Web Project J28.</p>
+    </div>
+    
+      
+       
+    </footer>
+  </body>
+</html>
